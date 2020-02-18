fdee6ee5f8f9aa3ef4739b6cb2128449a941a40a
Razvan Vancea <rvancea@deloittece.com>
2020-02-18 16:11:23 +0200

    create angular project

--- bike-ui/src/app/app-routing.module.ts
+++ bike-ui/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AdminComponent} from './components/admin/admin.component';
+import {HomeComponent} from './components/home/home.component';
+import {ViewRegistrationComponent} from './components/view-registration/view-registration.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+    {
+      path: 'admin/view/:id',
+      component: ViewRegistrationComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bike-ui/src/app/app.module.ts
+++ bike-ui/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BikeService} from './services/bike.service';
+import {HttpClientModule} from '@angular/common/http';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    ViewRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [BikeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
